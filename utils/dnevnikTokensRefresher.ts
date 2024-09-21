cd6cda565c621ac84cffbbce5a66d0ab859f55b9
Alexander Naydenov <AleX83Xpert@mail.ru>
2024-09-21 08:27:24 +0500

    Update dnevnikTokensRefresher.ts

--- utils/dnevnikTokensRefresher.ts
+++ utils/dnevnikTokensRefresher.ts
@@ -14,7 +14,11 @@ export async function startTokensRefresher(godContext: KeystoneContext, interval
   refreshInterval = setInterval(async () => {
     logger.info({ msg: 'refresh tokens' })
 
-    const expiredSoonUsers = await godContext.db.TelegramUser.findMany({
+    
+// !!!!! query instead of db
+
+    
+    const expiredSoonUsers = await godContext.query.TelegramUser.findMany({
       where: {
         dnevnikAccessTokenExpirationDate: { lte: dayjs().add(refreshBeforeSec, 'seconds').toISOString() },
       },
