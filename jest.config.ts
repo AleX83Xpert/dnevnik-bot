bf6b278888d63fdd41963481c02acbbbe0dd5a09
AleX83Xpert <AleX83Xpert@mail.ru>
2024-10-01 22:28:31 +0500

    jest was added

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+import type {Config} from 'jest'
+
+const config: Config = {
+  verbose: true,
+  testEnvironment: "node",
+  transform: {
+    "^.+.tsx?$": ["ts-jest", {}],
+  },
+  modulePathIgnorePatterns: [
+    '<rootDir>/pgdata',
+    '<rootDir>/node_modules'
+  ],
+};
+
+export default config
