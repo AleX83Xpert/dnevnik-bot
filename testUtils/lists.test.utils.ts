022b648c8ca7165047ad92cd2e7389907e4c89e1
Alexander Naydenov <AleX83Xpert@mail.ru>
2024-10-29 20:36:15 +0500

    remove `--watch` from test command (#1)
    * remove `--watch` from test command
    
    * uncomment starting containers
    
    * temporary comment some lines at workflow
    
    * no
    
    * `docker-compose` to `docker compose`
    
    * run only postgres and redis for ci tests
    
    * fix
    
    * npm ci -->> npm install
    
    * add build step
    
    * try open postgres port for tests
    
    * add step for prepare env file
    
    * override compose file for tests
    
    * disable next telemetry
    
    * remove comment from action

--- testUtils/lists.test.utils.ts
+++ testUtils/lists.test.utils.ts
@@ -1,5 +1,4 @@
 import { getContext } from '@keystone-6/core/context'
-import { resetDatabase } from '@keystone-6/core/testing'
 import * as PrismaModule from '.prisma/client'
 import config from '../keystone'
 
