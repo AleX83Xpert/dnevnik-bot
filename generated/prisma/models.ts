1f70f5aa7be3e8ce71557ce3fad1361236537383
AleX83Xpert <AleX83Xpert@mail.ru>
2026-08-23 16:27:27 +0500

    chore: generated files

--- generated/prisma/models.ts
+++ generated/prisma/models.ts
@@ -8,6 +8,6 @@
  *
  * 🟢 You can import this file directly.
  */
-export type * from './models/User.js'
-export type * from './models/MessengerUser.js'
-export type * from './commonInputTypes.js'
+export type * from './models/User.ts'
+export type * from './models/MessengerUser.ts'
+export type * from './commonInputTypes.ts'
